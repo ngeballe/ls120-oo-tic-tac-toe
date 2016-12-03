573620ac6d3c765b4c2a0d6bd88afe35f1c87107
Nick Geballe <ngeballe@gmail.com>
2016-12-03 11:55:40 -0500

    Refactoring. Make most methods in TTTGame private

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -96,6 +96,7 @@ end
 class TTTGame
   HUMAN_MARKER = 'X'.freeze
   COMPUTER_MARKER = 'O'.freeze
+  FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
 
@@ -103,7 +104,29 @@ class TTTGame
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
+    @current_marker = FIRST_TO_MOVE
   end
+
+  def play
+    clear
+    display_welcome_message
+    loop do
+      display_board
+
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_and_display_board if human_turn?
+      end
+      display_result
+      break unless play_again?
+      reset
+      display_play_again_message
+    end
+    display_goodbye_message
+  end
+
+  private
 
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
@@ -143,6 +166,20 @@ class TTTGame
     board[square] = computer.marker
   end
 
+  def current_player_moves
+    if human_turn?
+      human_moves
+      @current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_marker = HUMAN_MARKER
+    end
+  end
+
+  def human_turn?
+    @current_marker == HUMAN_MARKER
+  end
+
   def display_result
     clear_screen_and_display_board
     case board.winning_marker
@@ -173,39 +210,13 @@ class TTTGame
 
   def reset
     board.reset
+    @current_marker = FIRST_TO_MOVE
     clear
   end
 
   def display_play_again_message
     puts "Let's play again!"
     puts ""
-  end
-
-  def game_over?
-    board.someone_won? || board.full?
-  end
-
-  def play
-    clear
-    display_welcome_message
-    loop do
-      display_board
-
-      loop do
-        human_moves
-        break if game_over?
-
-        computer_moves
-        break if game_over?
-
-        clear_screen_and_display_board
-      end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
-    end
-    display_goodbye_message
   end
 end
 
